2219e52998911b9392044b96eb0d27c790a42a95
Fernando Leoncio Porfirio <fernando.lporfirio@gmail.com>
2024-01-27 15:29:29 -0300

    add entities in datasource

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -6,6 +6,20 @@ import { InsertDataInState1706359245735 } from './migrations/1706359245735-Inser
 import { InsertDataInCity1706359255544 } from './migrations/1706359255544-InsertDataInCity'
 import { InsertDataInEnrolmentStatus1706359269830 } from './migrations/1706359269830-InsertDataInEnrolmentStatus'
 
+import AccessLevel from '../app/entities/AccessLevel'
+import AccessLevelTransaction from '../app/entities/AccessLevelTransaction'
+import City from '../app/entities/City'
+import Class from '../app/entities/Class'
+import Course from '../app/entities/Course'
+import Enrolment from '../app/entities/Enrolment'
+import EnrolmentStatus from '../app/entities/EnrolmentStatus'
+import Institution from '../app/entities/Institution'
+import State from '../app/entities/State'
+import Student from '../app/entities/Student'
+import Transaction from '../app/entities/Transaction'
+import User from '../app/entities/User'
+import UserAccessLevel from '../app/entities/UserAccessLevel'
+
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: 'localhost',
@@ -16,7 +30,21 @@ export const AppDataSource = new DataSource({
   database: 'teste',
   synchronize: true,
   logging: false,
-  entities: [],
+  entities: [
+    AccessLevel,
+    AccessLevelTransaction,
+    City,
+    Class,
+    Course,
+    Enrolment,
+    EnrolmentStatus,
+    Institution,
+    State,
+    Student,
+    Transaction,
+    User,
+    UserAccessLevel
+  ],
   migrations: [
     CreateTables1706358632771,
     InsertDataInState1706359245735,
