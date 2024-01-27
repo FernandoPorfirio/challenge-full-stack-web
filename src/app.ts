7697be4a4a14b37e3a31b35ce4d62aac85be4fe6
Fernando Leoncio Porfirio <fernando.lporfirio@gmail.com>
2024-01-27 12:54:49 -0300

    fix with prettier and eslint

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,23 @@
-import "reflect-metadata";
-import express from "express";
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import compression from "compression";
-import cors from "cors";
+import 'reflect-metadata'
+import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compression from 'compression'
+import cors from 'cors'
 
-import { router } from "./app/routes/index";
+import { router } from './app/routes/index'
 
-export const app = express();
+export const app = express()
 
 app.use(
   cors({
-    credentials: true,
+    credentials: true
   })
-);
+)
 
-app.use(express.json());
-app.use(compression());
-app.use(cookieParser());
-app.use(bodyParser.json());
+app.use(express.json())
+app.use(compression())
+app.use(cookieParser())
+app.use(bodyParser.json())
 
-app.use("/", router);
+app.use('/', router)
--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,19 +1,19 @@
-import "reflect-metadata";
-import {DataSource} from "typeorm";
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
 
-import { CreateTables1706358632771 } from "./migrations/1706358632771-CreateTables";
-import { InsertDataInState1706359245735 } from "./migrations/1706359245735-InsertDataInState";
-import { InsertDataInCity1706359255544 } from "./migrations/1706359255544-InsertDataInCity";
-import { InsertDataInEnrolmentStatus1706359269830 } from "./migrations/1706359269830-InsertDataInEnrolmentStatus";
+import { CreateTables1706358632771 } from './migrations/1706358632771-CreateTables'
+import { InsertDataInState1706359245735 } from './migrations/1706359245735-InsertDataInState'
+import { InsertDataInCity1706359255544 } from './migrations/1706359255544-InsertDataInCity'
+import { InsertDataInEnrolmentStatus1706359269830 } from './migrations/1706359269830-InsertDataInEnrolmentStatus'
 
 export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: "localhost",
+  type: 'postgres',
+  host: 'localhost',
   port: 5432,
-  username: "postgres",
-  password: "d20m07",
+  username: 'postgres',
+  password: 'd20m07',
   // database: "educacao",
-  database: "teste",
+  database: 'teste',
   synchronize: true,
   logging: false,
   entities: [],
@@ -23,6 +23,5 @@ export const AppDataSource = new DataSource({
     InsertDataInCity1706359255544,
     InsertDataInEnrolmentStatus1706359269830
   ],
-  subscribers: [] 
-
+  subscribers: []
 })
