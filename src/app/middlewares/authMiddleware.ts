759ccd13c102a030e4a77396fa08269391e3ed4b
Fernando Leoncio Porfirio <fernando.lporfirio@gmail.com>
2024-01-28 10:20:03 -0300

    fix name

---
+++ src/app/middlewares/authMiddleware.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+dotenv.config()
+// import IToken from '../interfaces/IToken'
+
+export default function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { authorization } = req.headers
+
+  if (!authorization) {
+    return res.status(401).json({ error: 'Invalid token' })
+  }
+
+  const token = authorization.replace('Bearer', '').trim()
+
+  try {
+    jwt.verify(token, process.env.JWT_SECRET || 'secret')
+    next()
+  } catch {
+    return res.status(401).json({ error: 'Invalid token' })
+  }
+}
