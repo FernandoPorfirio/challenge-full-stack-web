81fb8fcc5ba0cd84c2b296e764bda425f0d77677
Fernando Leoncio Porfirio <fernando.lporfirio@gmail.com>
2024-01-28 21:15:23 -0300

    add user service

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -1,56 +1,17 @@
-import { Request, Response, Router } from 'express'
-import * as yup from 'yup'
+// controllers/UserController.ts
 
-import { ConflictError, BadRequestError } from '../helper/ApiError'
-// import User from '../model/User'
-import UserRepository from '../repositories/UserRepository'
-// import IUser from '../interfaces/IUser'
+import { Request, Response } from 'express'
+import UserService from '../services/UserService'
 
 class UserController {
   async getAllUsers(req: Request, res: Response): Promise<Response> {
-    const user = await UserRepository.getUser()
-    return res.status(200).json(user)
+    const users = await UserService.getAllUsers()
+    return res.status(200).json(users)
   }
 
   async createUser(req: Request, res: Response): Promise<Response> {
-    const bodyValidation: yup.ObjectSchema<any> = yup.object().shape({
-      name: yup
-        .string()
-        .required('Name is required')
-        .min(3, 'Name must be at least 3 characters')
-        .max(50, 'Name must be at most 50 characters'),
-      email: yup
-        .string()
-        .required('Email is required')
-        .min(4, 'Email must be at least 4 characters')
-        .max(320, 'Email must be at most 320 characters')
-        .email('Invalid email format'),
-      password: yup
-        .string()
-        .required('Password is required')
-        .max(60, 'Password must be at most 60 characters'),
-      createdat: yup.date().default(() => new Date()),
-      actived: yup.bool().default(true)
-    })
-
-    try {
-      await bodyValidation.validate(req.body)
-    } catch (error) {
-      const yupError = error as yup.ValidationError
-      throw new BadRequestError(yupError.message)
-    }
-
-    const { id, name, email } = req.body
-    const existEmail = await UserRepository.findUserByEmail(email)
-
-    if (existEmail) {
-      throw new ConflictError('Email address already exists')
-    }
-
-    await UserRepository.createUser(req.body)
-
-    const userResponse = { id, name, email }
-
+    const userData = req.body
+    const userResponse = await UserService.createUser(userData)
     return res.status(200).json(userResponse)
   }
 }
