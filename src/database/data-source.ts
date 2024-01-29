d16eca278c15d19de63d85784e6d7bb09ddb34c4
Fernando Leoncio Porfirio <fernando.lporfirio@gmail.com>
2024-01-29 19:27:45 -0300

    refactor:  data-source separate imports of entities and migrations

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -2,24 +2,9 @@ import 'reflect-metadata'
 import dotenv from 'dotenv'
 import { DataSource } from 'typeorm'
 
-import { CreateTables1706358632771 } from './migrations/1706358632771-CreateTables'
-import { InsertDataInState1706359245735 } from './migrations/1706359245735-InsertDataInState'
-import { InsertDataInCity1706359255544 } from './migrations/1706359255544-InsertDataInCity'
-import { InsertDataInEnrolmentStatus1706359269830 } from './migrations/1706359269830-InsertDataInEnrolmentStatus'
+import { entities } from './entities'
 
-import AccessLevel from '../app/model/AccessLevel'
-import AccessLevelTransaction from '../app/model/AccessLevelTransaction'
-import City from '../app/model/City'
-import Class from '../app/model/Class'
-import Course from '../app/model/Course'
-import Enrolment from '../app/model/Enrolment'
-import EnrolmentStatus from '../app/model/EnrolmentStatus'
-import Institution from '../app/model/Institution'
-import State from '../app/model/State'
-import Student from '../app/model/Student'
-import Transaction from '../app/model/Transaction'
-import User from '../app/model/User'
-import UserAccessLevel from '../app/model/UserAccessLevel'
+import { migrations } from './migrations'
 
 dotenv.config()
 
@@ -32,26 +17,7 @@ export const AppDataSource = new DataSource({
   database: process.env.TYPEORM_DATABASE,
   synchronize: true,
   logging: false,
-  entities: [
-    AccessLevel,
-    AccessLevelTransaction,
-    City,
-    Class,
-    Course,
-    Enrolment,
-    EnrolmentStatus,
-    Institution,
-    State,
-    Student,
-    Transaction,
-    User,
-    UserAccessLevel
-  ],
-  migrations: [
-    CreateTables1706358632771,
-    InsertDataInState1706359245735,
-    InsertDataInCity1706359255544,
-    InsertDataInEnrolmentStatus1706359269830
-  ],
+  entities,
+  migrations,
   subscribers: []
 })
