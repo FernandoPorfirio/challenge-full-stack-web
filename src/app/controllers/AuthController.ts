7020c496d3fc5008275f78d6861e86ab67b6a396
Fernando Leoncio Porfirio <fernando.lporfirio@gmail.com>
2024-01-28 21:12:22 -0300

    add auth service

--- src/app/controllers/AuthController.ts
+++ src/app/controllers/AuthController.ts
@@ -1,41 +1,16 @@
-import { Request, Response } from 'express'
-import { UnauthorizedError } from '../helper/ApiError'
-import UserRepository from '../repositories/UserRepository'
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-import dotenv from 'dotenv'
+// controllers/AuthController.ts
 
-dotenv.config()
+import { Request, Response } from 'express'
+import AuthService from '../services/AuthService'
 
 class AuthController {
   async auth(req: Request, res: Response): Promise<Response> {
-
     const { email, password } = req.body
-    const user = await UserRepository.findUserByEmail(email)
 
-    if (!user) {
-      throw new UnauthorizedError('Invalid credentials')
-    }
+    const authResult = await AuthService.authenticate(email, password)
 
-    const isValidPassword = await bcrypt.compare(password, user.password)
-
-    if (!isValidPassword) {
-      throw new UnauthorizedError('Invalid credentials')
-    }
-
-    const token = jwt.sign(
-      { id: user.id },
-      process.env.JWT_SECRET || 'secret',
-      { expiresIn: '1d' }
-    )
-
-    return res.status(200).json({
-      user: {
-        name: user.name,
-        email: user.email
-      },
-      token
-    })
+    return res.status(200).json(authResult)
   }
 }
+
 export default new AuthController()
