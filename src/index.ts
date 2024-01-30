c09b3ff91b04285a013d12eb399474b15a46f3b3
Fernando Leoncio Porfirio <fernando.lporfirio@gmail.com>
2024-01-30 11:49:57 -0300

    feat: add datasourceOptions dependence in DatabaseManager instance

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
+import { DataSourceOptions } from 'typeorm'
 import app from './app'
 
 import dotenv from 'dotenv'
+import DatabaseManager from '../src/database/databaseManager'
+
 dotenv.config()
 
 const port = process.env.PORT || 3000
+
+const datasourceOptions: DataSourceOptions = {
+  type: process.env.TYPEORM_CONNECTION as any,
+  host: process.env.TYPEORM_HOST,
+  port: parseInt(process.env.TYPEORM_PORT || '3000', 10),
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  synchronize: true,
+  logging: false
+}
+
+const dbManager: DatabaseManager = new DatabaseManager(datasourceOptions)
+dbManager.initializeDataSource()
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
