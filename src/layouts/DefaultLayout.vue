adcead967a889b89b13574de2d2fda0598070ff6
Fernando Leoncio Porfirio <fernando.lporfirio@gmail.com>
2024-02-03 18:59:46 -0300

    feat: add layout files

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,32 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppContent from './components/AppContent.vue'
+import AppFooter from './components/AppFooter.vue'
+
+export default {
+  name: 'DefaultLayout',
+  components: {
+    AppHeader,
+    AppContent,
+    AppFooter
+  }
+}
+</script>
+
+<template>
+  <div>
+    <header>
+      <AppHeader />
+    </header>
+
+    <main>
+      <AppContent>
+        <router-view></router-view>
+      </AppContent>
+    </main>
+
+    <footer>
+      <AppFooter />
+    </footer>
+  </div>
+</template>
