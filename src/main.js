4e1598cd75670246136ad7e0f57ff7ffbe436d53
Fernando Leoncio Porfirio <fernando.lporfirio@gmail.com>
2024-02-03 12:19:56 -0300

    feat: add router and vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import { createApp } from 'vue'
+import { createStore } from 'vuex'
 import App from './App.vue'
+import router from './router'
+import authModule from './store/auth';
 
-// Vuetify
+
+const store = createStore({
+  modules: {
+    auth: authModule,
+  }
+})
+
+store.dispatch('auth/loadAuthToken');
+
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
@@ -9,7 +20,7 @@ import * as directives from 'vuetify/directives'
 
 const vuetify = createVuetify({
   components,
-  directives,
+  directives
 })
 
-createApp(App).use(vuetify).mount('#app')
+createApp(App).use(vuetify).use(router).use(store).mount('#app')
