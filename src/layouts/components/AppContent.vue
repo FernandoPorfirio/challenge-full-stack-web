857c444b837e6986555e4dea403b2004304206cd
Fernando Leoncio Porfirio <fernando.lporfirio@gmail.com>
2024-02-03 19:00:02 -0300

    feat: add layout components

---
+++ src/layouts/components/AppContent.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'AppContent'
+}
+</script>
+
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<style></style>
---
+++ src/layouts/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+import AppNavigation from './AppNavigation.vue'
+
+export default {
+  name: 'AppHeader',
+  components: {
+    AppNavigation
+  }
+}
+</script>
+
+<template>
+  <div>
+      <AppNavigation />
+  </div>
+</template>
+
+<style></style>
