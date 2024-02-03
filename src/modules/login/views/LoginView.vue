7186ea6703697032cef5a3685330caecd1b1bd8f
Fernando Leoncio Porfirio <fernando.lporfirio@gmail.com>
2024-02-03 15:20:58 -0300

    feat: add login view in module

---
+++ src/modules/login/views/LoginView.vue
@@ -0,0 +1,23 @@
+<!-- SeuComponente.vue -->
+
+<template>
+  <div>
+    <h1>{{ mensagem }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginView',
+
+  data() {
+    return {
+      mensagem: 'LoginView'
+    }
+  },
+  methods: {},
+  mounted() {}
+}
+</script>
+
+<style scoped></style>
