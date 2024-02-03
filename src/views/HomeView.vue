af46d147f13c66c760d88e01f4f2c508643c6954
Fernando Leoncio Porfirio <fernando.lporfirio@gmail.com>
2024-02-03 15:21:32 -0300

    feat: add home view

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<!-- SeuComponente.vue -->
+
+<template>
+  <div>
+    <h1>{{ mensagem }}</h1>
+    <router-link :to="{ name: 'login' }">Login</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+
+  data() {
+    return {
+      mensagem: 'HomeView'
+    }
+  },
+  methods: {},
+  mounted() {}
+}
+</script>
+
+<style scoped></style>
