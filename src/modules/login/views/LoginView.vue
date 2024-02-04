65a5cfc02362b41e4b9135c0687d2698e0bba25e
Fernando Leoncio Porfirio <fernando.lporfirio@gmail.com>
2024-02-04 16:19:13 -0300

    feat: add login form in login view

--- src/modules/login/views/LoginView.vue
+++ src/modules/login/views/LoginView.vue
@@ -1,20 +1,38 @@
 <!-- SeuComponente.vue -->
 
 <template>
-  <div>
-    <h1>{{ mensagem }}</h1>
+  <div class="text-subtitle-1 text-medium-emphasis">Account</div>
+
+  <v-text-field
+    density="compact"
+    placeholder="Email address"
+    prepend-inner-icon="mdi-email-outline"
+    variant="outlined"
+  ></v-text-field>
+
+  <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+    Password
   </div>
+
+  <v-text-field
+    :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+    :type="visible ? 'text' : 'password'"
+    density="compact"
+    placeholder="Enter your password"
+    prepend-inner-icon="mdi-lock-outline"
+    variant="outlined"
+    @click:append-inner="visible = !visible"
+  ></v-text-field>
+
+  <v-btn block class="mb-8" color="blue" size="large" variant="tonal"> Log In </v-btn>
 </template>
 
 <script>
 export default {
   name: 'LoginView',
-
-  data() {
-    return {
-      mensagem: 'LoginView'
-    }
-  },
+  data: () => ({
+    visible: false
+  }),
   methods: {},
   mounted() {}
 }
