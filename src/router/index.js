d85e412cace83e11904ce57ff4d10ed3037408b8
Fernando Leoncio Porfirio <fernando.lporfirio@gmail.com>
2024-02-04 17:33:42 -0300

    fix: remove console log

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,6 @@ import {HOME} from '@/constants/router'
 
 import LoginRoutes from '@/modules/login/router';
 
-console.log([HOME, LoginRoutes]);
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
