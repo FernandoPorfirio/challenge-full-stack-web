ac635e104c0ae7d69a682b3f93de03a2d360279f
Fernando Leoncio Porfirio <fernando.lporfirio@gmail.com>
2024-02-05 18:41:23 -0300

    refactor: change template and script order

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,23 +1,3 @@
-<template>
-  <div>
-    <app-bar @toggle-drawer="toggleDrawer" />
-    <v-navigation-drawer app v-model="drawer">
-      <!-- Conteúdo do menu aqui -->
-      <v-list>
-        <v-list-item v-for="(link, index) in links" :key="index" :to="link.to">
-          <template v-slot:prepend>
-            <v-icon :icon="link.icon"></v-icon>
-          </template>
-          <v-list-item-title>{{ link.text }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
-      <router-view></router-view>
-    </v-main>
-  </div>
-</template>
-
 <script>
 import AppBar from '@/layouts/components/AppBar.vue'
 
@@ -42,3 +22,22 @@ export default {
   }
 }
 </script>
+
+<template>
+  <div>
+    <app-bar @toggle-drawer="toggleDrawer" />
+    <v-navigation-drawer app v-model="drawer">
+      <v-list>
+        <v-list-item v-for="(link, index) in links" :key="index" :to="link.to">
+          <template v-slot:prepend>
+            <v-icon :icon="link.icon"></v-icon>
+          </template>
+          <v-list-item-title>{{ link.text }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px">
+      <router-view></router-view>
+    </v-main>
+  </div>
+</template>
