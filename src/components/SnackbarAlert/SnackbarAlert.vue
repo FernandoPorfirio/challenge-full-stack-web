14a87f93ed42354f3485e1c5cd7d9946cefef56c
Fernando Leoncio Porfirio <fernando.lporfirio@gmail.com>
2024-02-04 21:36:10 -0300

    refactor: formating code

--- src/components/SnackbarAlert/SnackbarAlert.vue
+++ src/components/SnackbarAlert/SnackbarAlert.vue
@@ -1,32 +1,23 @@
 <template>
-  <v-snackbar
-      v-model="snackbar.show"
-      :timeout="5000"
-    >
-      {{ snackbar.message }}
+  <v-snackbar v-model="snackbar.show" :timeout="5000">
+    {{ snackbar.message }}
 
-      <template v-slot:actions>
-        <v-btn
-          color="pink"
-          variant="text"
-          @click="snackbar.show = false"
-        >
-          X
-        </v-btn>
-      </template>
-    </v-snackbar>
+    <template v-slot:actions>
+      <v-btn color="pink" variant="text" @click="snackbar.show = false"> X </v-btn>
+    </template>
+  </v-snackbar>
 </template>
 
 <script>
-import SnackbarService from '@/components/SnackbarAlert/service.js';
+import SnackbarService from '@/components/SnackbarAlert/service.js'
 
 export default {
   name: 'SnackbarAlert',
   setup() {
-    const snackbar = SnackbarService.getSnackbar();
+    const snackbar = SnackbarService.getSnackbar()
     return {
-      snackbar,
-    };
-  },
-};
+      snackbar
+    }
+  }
+}
 </script>
