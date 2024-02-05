ba5383101ce0c580caf54be943a8a1e0efe0b171
Fernando Leoncio Porfirio <fernando.lporfirio@gmail.com>
2024-02-05 18:38:40 -0300

    feat: add new methods in student service

--- src/modules/student/services.js
+++ src/modules/student/services.js
@@ -1,15 +1,44 @@
 import http from '@/services/http'
-import authHeader from '@/services/authHeader';
+import authHeader from '@/services/authHeader'
+import { STUDENT_CREATE } from '@/constants/router'
+
+import router from '@/router'
 
 class StudentService {
   getAllStudents() {
-    console.log({ headers: authHeader() });
-    return http.post('/student',{ headers: authHeader() }).then((response) => {
+    const headers = authHeader()
+    return http.get('/student', { headers }).then((response) => {
       return response.data
     })
   }
 
+  createStudent(student) {
+    const headers = authHeader()
+
+    if (student.id) {
+      return http.put(`/student/${student.id}`, student, { headers }).then((response) => {
+        sessionStorage.removeItem('student')
+        return response.data
+      })
+    }
+    return http.post('/student', student, { headers }).then((response) => {
+      return response.data
+    })
+  }
+
+  deleteStudent(id) {
+    const headers = authHeader()
+
+    return http.delete(`/student/${id}`, { headers }).then((response) => {
+      sessionStorage.removeItem('student')
+      return response.data
+    })
+  }
+
+  updateStudent(student) {
+    sessionStorage.setItem('student', JSON.stringify(student))
+    router.push(STUDENT_CREATE.path)
+  }
 }
 
 export default new StudentService()
-
