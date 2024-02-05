43a1b018d75858fa65eae7dfc5586d2b25286d8f
Fernando Leoncio Porfirio <fernando.lporfirio@gmail.com>
2024-02-05 18:36:05 -0300

    fix: remove unused template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,23 +2,5 @@
 
 <template>
   <div>
-    <h1>{{ mensagem }}</h1>
-    <router-link :to="{ name: 'login' }">Login</router-link>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'HomeView',
-
-  data() {
-    return {
-      mensagem: 'HomeView'
-    }
-  },
-  methods: {},
-  mounted() {}
-}
-</script>
-
-<style scoped></style>
